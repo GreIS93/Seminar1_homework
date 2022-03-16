f8a1750f952edf45a533e75de0ba4ee205cdac82
Ilya Grekov <grekovis93@gmail.com>
2022-03-16 23:37:54 +0700

    save1

---
+++ homework/task2/Program.cs
@@ -0,0 +1,20 @@
+﻿
+Console.WriteLine("Введите число a:");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b:");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число c:");
+int c = int.Parse(Console.ReadLine());
+int max = 0;
+if (a>b; a>c)
+{
+    Console.WriteLine("Наибольшее число " + a);
+}
+if (b>a; b>c)
+{
+    Console.WriteLine("Наибольшее число " + b);
+}
+if (c>b; c>a)
+{
+    Console.WriteLine("Наибольшее число " + c);
+}
---
+++ homework/task3/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите число");
+int n = int.Parse(Console.ReadLine());
+int i = 0;
+int n1 = n;
+if (n == 0) Console.Write("Число " + n + " не является четным");
+if (n % 2 == 0) Console.Write("Число " + n + " является четным");
