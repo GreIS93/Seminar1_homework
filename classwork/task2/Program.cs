5737625217e8e5ba800bc1adb08c75fbe10f8269
Ilya Grekov <grekovis93@gmail.com>
2022-03-17 16:36:31 +0700

    cwtask2

---
+++ classwork/task2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Сравнить 2 числа
+Console.WriteLine("Введите число a:");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число b:");
+int b = int.Parse(Console.ReadLine());
+
+if (a == b)
+{
+    Console.WriteLine("Число " + a + " равно числу " + b);
+}
+else if (a>b)
+    {
+        Console.WriteLine("Число " + a + " больше числа " + b);
+    }
+    else 
+    {
+        Console.WriteLine("Число " + a + " меньше числа " + b);
+    }
