514e4c3a7507bdc6d49cfd0e74fbd7909059fa7e
Ilya Grekov <grekovis93@gmail.com>
2022-03-17 13:08:21 +0700

    cwtask1

--- classwork/task1/Program.cs
+++ classwork/task1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Является ли первое число квадратом второго
+Console.WriteLine("Введите число a:");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число b:");
+int b = int.Parse(Console.ReadLine());
+
+if (a == b*b)
+{
+    Console.WriteLine("Число " + a + " является квадратом числа " + b);
+}
+else 
+{
+    Console.WriteLine("Число " + a + " не является квадратом числа " + b);
+}
