48f09a180b30f9ab9b5057973a1161ed44005501
Ilya Grekov <grekovis93@gmail.com>
2022-03-18 16:43:53 +0700

    hwtask6

--- homework/task6/Program.cs
+++ homework/task6/Program.cs
@@ -1,2 +1,12 @@
 ﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+int num = new Random().Next(10, 99);
+Console.WriteLine("Дано число: " + num);
 
+if ((num / 10) > (num % 10))
+{
+    Console.WriteLine("Наибольшая цифра в числе: " + num / 10);
+}
+else
+{
+    Console.WriteLine("Наибольшая цифра в числе: " + num % 10);
+}
