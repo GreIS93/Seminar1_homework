d9faf87fdfe63e1f2042a05538a8dad336428354
Ilya Grekov <grekovis93@gmail.com>
2022-03-18 16:33:53 +0700

    hwtask5

---
+++ homework/task5/Program.cs
@@ -0,0 +1,9 @@
+﻿// Показать вторую цифру трёхзначного числа
+
+Console.WriteLine("Введите трехзначное число");
+int n = int.Parse(Console.ReadLine());
+
+if (100 <= n && n <= 1000)
+{
+    Console.WriteLine((n % 100)/10);
+}
