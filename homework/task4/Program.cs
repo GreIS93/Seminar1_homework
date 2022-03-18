2a0d2de0241c46002ae35f8305684256dc68dc5d
Ilya Grekov <grekovis93@gmail.com>
2022-03-18 16:22:14 +0700

    hwtask4

--- homework/task4/Program.cs
+++ homework/task4/Program.cs
@@ -1,9 +1,8 @@
 ﻿// Показать последнюю цифру трёхзначного числа
-Console.WriteLine("Введите трехзначное число num");
+Console.WriteLine("Введите трехзначное число");
+int n = int.Parse(Console.ReadLine());
 
-int num = int.Parse(Console.ReadLine());
-
-int m = 0;
-
-
-Console.WriteLine("Последняя цифра в числе num: " + m);
+if (100 <= n && n <= 1000)
+{
+    Console.WriteLine(n % 10);
+}
